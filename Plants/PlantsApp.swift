141071381ad75cad92e3b59e12f6998189f1fb4a
Faye8saud <fayii8866@gmail.com>
2025-10-19 14:28:08 +0300

    Initial Commit

---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Fay  on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
